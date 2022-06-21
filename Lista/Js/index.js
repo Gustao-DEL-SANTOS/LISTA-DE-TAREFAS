048c6228c888d9cac919ad6d13d1bb30a8924bd8
Gustavo <gustavodelsantos15@gmail.com>
2022-06-21 19:18:22 -0300

    lista de tarefas finalizada canal: Programação Web

--- Lista/Js/index.js
+++ Lista/Js/index.js
@@ -1,3 +1,4 @@
+let contador = 0
 let input = document.getElementById('txtTarefas')
 let btnAdd = document.getElementById('btnAdd')
 let main = document.getElementById('areaLista')
@@ -7,16 +8,21 @@ function addTarefa(){
     let varInput = input.value;
 
     if((varInput !== "") && (varInput !== null) && (varInput !== undefined)){
-        let novoItem = `
-        <div class="item">
-            <div class="item-icone">
-                <i class="mdi mdi-circle-outline"></i>
+        
+        ++contador;
+
+        let novoItem = 
+        `<div id=${contador} class="item">
+            <div class="item-icone" onclick="marcarTarefa(${contador})">
+                <i id="icone_${contador}"class="mdi mdi-circle-outline"></i>
             </div>
-            <div class="item-nome">
+            <div class="item-nome" onclick="marcarTarefa(${contador})">
                 ${varInput}
             </div>
             <div class="item-botao">
-                <button class="btnDelete" onclick="deletar()"><i class="mdi mdi-delete"></i>Deletar</button>
+                <button class="btnDelete" onclick="deletar(${contador})"><i class="mdi mdi-delete"></i>
+                    Deletar
+                </button>
             </div>
         </div>`;
         main.innerHTML += novoItem;
@@ -32,4 +38,30 @@ input.addEventListener('keyup', function(event){
         btnAdd.click();
     }
 })
+
+function deletar(idDeletar){
+    let deleta = document.getElementById(idDeletar)
+    deleta.remove();
+}
+
+function marcarTarefa(id){
+    let item = document.getElementById(id)
+    item.getAttribute('class')
+
+    if(item.getAttribute('class') == 'item'){
+        item.classList.add('clicado')
+        let icone = document.getElementById('icone_'+id)
+        icone.classList.remove('mdi-circle-outline')
+        icone.classList.add('mdi-check-circle')
+        // issp colocara a tarefa no final da lista
+        item.parentNode.appendChild(item)
+        
+    }else{
+        item.classList.remove('clicado')
+        let icone = document.getElementById('icone_'+id)        
+        icone.classList.remove('mdi-check-circle')
+        icone.classList.add('mdi-circle-outline')
+    }
+
+}
 
